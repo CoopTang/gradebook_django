1faf64566e06102ab54bf3349e63bea06647cddd
Michael Cooper <macoop2363@gmail.com>
2020-04-01 21:52:28 -0600

    Add many to many relationship between Sections and Students

--- courses/models.py
+++ courses/models.py
@@ -30,8 +30,6 @@ class Homework(models.Model):
   def __str__(self):
     return self.title
 
-    from django.db import models
-
 class Student(models.Model):
   first_name = models.CharField(max_length=200)
   last_name  = models.CharField(max_length=200)
@@ -42,6 +40,7 @@ class Student(models.Model):
 class Section(models.Model):
   section_id = models.CharField(max_length = 5, default = '001')
   course     = models.ForeignKey(Course, on_delete = models.CASCADE)
+  students   = models.ManyToManyField(Student)
 
   def __str__(self):
     return '%s-%s' % (self.course.name, self.section_id)
