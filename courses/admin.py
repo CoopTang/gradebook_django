dde1ad20d9ddfe8234f898ff86ee1358ad2ce14e
Michael Cooper <macoop2363@gmail.com>
2020-04-01 20:38:50 -0600

    Add Quiz admin functionality

--- courses/admin.py
+++ courses/admin.py
@@ -1,18 +1,29 @@
 from django.contrib import admin
 
-from .models import Course, Test
+from .models import Course, Test, Quiz
 
 class TestAdmin(admin.ModelAdmin):
-  list_display = ['title']
-  fields = ['title']
+  list_display = ['title', 'total_points']
+  fields = ['title', 'total_points', 'course']
 
 class TestInline(admin.TabularInline):
   model = Test
+  
+
+class QuizAdmin(admin.ModelAdmin):
+  list_display = ['title', 'total_points']
+  fields = ['title', 'total_points', 'course']
+
+class QuizInline(admin.TabularInline):
+  model = Quiz
+
 class CourseAdmin(admin.ModelAdmin):
   model = Course
   inlines = [
     TestInline,
+    QuizInline
   ]
 
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Test, TestAdmin)
+admin.site.register(Quiz, QuizAdmin)
