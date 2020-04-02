3ec6439abbd244c169aa398841152ddd6b040e70
Michael Cooper <macoop2363@gmail.com>
2020-04-01 20:49:03 -0600

    Add Quiz model to Courses package

--- courses/models.py
+++ courses/models.py
@@ -21,3 +21,11 @@ class Quiz(models.Model):
 
   def __str__(self):
     return self.title
+
+class Homework(models.Model):
+  title  = models.CharField(max_length = 200)
+  total_points = models.PositiveIntegerField(default = 1)
+  course = models.ForeignKey(Course, on_delete = models.CASCADE)
+
+  def __str__(self):
+    return self.title
