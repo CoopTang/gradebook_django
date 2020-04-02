eb963a7cb1528227715cfaa36088394776572486
Michael Cooper <macoop2363@gmail.com>
2020-04-01 19:18:35 -0600

    Fix typo in Course field

--- courses/models.py
+++ courses/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Course(models.Model):
-  name = models.CharField(max_lenth = 200)
+  name = models.CharField(max_length = 200)
 
 class Test(models.Model):
   title  = models.CharField(max_length = 200)
