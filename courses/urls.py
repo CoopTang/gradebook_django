7af3bd6e2b4a3873f0484e3cc92e093582b4da8a
Michael Cooper <macoop2363@gmail.com>
2020-04-02 16:15:29 -0600

    Add route for a Course section show page

--- courses/urls.py
+++ courses/urls.py
@@ -7,4 +7,5 @@ app_name = 'courses'
 urlpatterns = [
   path('', views.index, name = 'index'),
   path('<int:course_id>/', views.show, name = 'show'),
+  path('<int:course_id>/sections/<int:section_id>/', views.sections, name = 'sections'),
 ]
