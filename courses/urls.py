6ab47ac3cdcba753b1beb037000632c8d2e5f93a
Michael Cooper <macoop2363@gmail.com>
2020-04-02 15:36:40 -0600

    Add namespace to routes

--- courses/urls.py
+++ courses/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = 'courses'
+
 urlpatterns = [
   path('', views.index, name = 'index'),
   path('<int:course_id>/', views.show, name = 'show'),
