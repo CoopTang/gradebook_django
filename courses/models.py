b0de507aa1fa42a1d538a49e77c80d872e372d92
Michael Cooper <macoop2363@gmail.com>
2020-04-01 19:58:17 -0600

    Add display for Course and Test

--- courses/models.py
+++ courses/models.py
@@ -3,6 +3,12 @@ from django.db import models
 class Course(models.Model):
   name = models.CharField(max_length = 200)
 
+  def __str__(self):
+    return self.name
+
 class Test(models.Model):
   title  = models.CharField(max_length = 200)
   course = models.ForeignKey(Course, on_delete = models.CASCADE)
+
+  def __str__(self):
+    return self.title
