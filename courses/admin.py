46a60782dda30c4798cd863c85b0cb0ddd817cb2
Michael Cooper <macoop2363@gmail.com>
2020-04-01 19:58:45 -0600

    Add admin for Course and Test

--- courses/admin.py
+++ courses/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Course, Test
+
+class TestAdmin(admin.ModelAdmin):
+  list_display = ['title']
+  fields = ['title']
+
+class TestInline(admin.TabularInline):
+  model = Test
+class CourseAdmin(admin.ModelAdmin):
+  model = Course
+  inlines = [
+    TestInline,
+  ]
+
+admin.site.register(Course, CourseAdmin)
+admin.site.register(Test, TestAdmin)
