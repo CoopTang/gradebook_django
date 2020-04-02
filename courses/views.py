f545920a96b652cade0f0617939a7db4fe4a4a4d
Michael Cooper <macoop2363@gmail.com>
2020-04-02 15:24:27 -0600

    Add views for Course show and index

--- courses/views.py
+++ courses/views.py
@@ -1,5 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 
+from .models import Course
+
 def index(request):
-  return HttpResponse("Hello, world! You're at the Courses index.")
+  courses = Course.objects.all()
+  context = {'courses': courses}
+  return render(request, 'courses/index.html', context)
+
+def show(request, course_id):
+  course = get_object_or_404(Course, pk = course_id)
+  context = {
+    'course': course,
+    'course_sections': course.section_set.all(),
+  }
+  return render(request, 'courses/show.html', context)
