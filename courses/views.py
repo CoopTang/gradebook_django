9cbc7fd3caaf522fa11a74d6309c7d0e5f6d34d4
Michael Cooper <macoop2363@gmail.com>
2020-04-02 16:15:09 -0600

    Add sections view for a Course section

--- courses/views.py
+++ courses/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
 
-from .models import Course
+from .models import Course, Section, Test, Quiz, Homework
 
 def index(request):
   courses = Course.objects.all()
@@ -15,3 +15,14 @@ def show(request, course_id):
     'course_sections': course.section_set.all(),
   }
   return render(request, 'courses/show.html', context)
+
+def sections(request, course_id, section_id):
+  tests = Test.objects.filter(course_id = course_id)
+  quizes = Quiz.objects.filter(course_id = course_id)
+  homeworks = Homework.objects.filter(course_id = course_id)
+  context = {
+    'tests': tests,
+    'quizes': quizes,
+    'homeworks': homeworks
+  }
+  return render(request, 'courses/sections/show.html', context)
